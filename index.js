491e0c254c80f8b8a9a2ea1134aabe896739d5a7
Lazzzare <96971788+Lazzzare@users.noreply.github.com>
2023-02-16 21:16:17 +0400

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 import express from "express";
+
+const books = [
+  {
+    isbn: "213123",
+    title: "ქარავანი",
+    author: "ჯემალ ქარჩხაძე",
+  },
+  {
+    isbn: "53231",
+    title: "გზაზე ერთი კაცი მიდიოდა",
+    author: "ოთარ ჭილაძე",
+  },
+  {
+    isbn: "09987",
+    title: "სამოსელი პირველი",
+    author: "გურამ დოჩანაშვილი",
+  },
+];
+
+const app = express();
+const port = 4001;
+
+app.get("/", (req, res) => {
+  res.send("Books API");
+});
+
+app.get("/books", (req, res) => {
+  res.json(books);
+});
+
+app.listen(port, () => {
+  console.log(`Started server on port: ${port}`);
+});
